da8dc8e425387604011a30f803174d6a9abc4d05
Amabeli_Nicco <Amabeli_Nicco@student.mahoningctc.com>
2020-12-09 13:43:38 -0500

    first commit

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,22 @@
+// Enter your code below to edit index.html
+
+yourName = "Nicco Amabeli"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "Hi, Im Nicco and Im 16, I am currently attending MCCTC, The Lab im in is Software Engineering and development."
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeReason = "I would like to go to Youngstown State if any other, I would most want to go for a bachalors in education or a bacholars in Game Design."
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+careerReason = "I think that i would want to either be a middle/Jr. High history teacher or have a job at Rockstar Games."
+document.getElementById("careerReason").innerHTML = careerReason;
+
+answerOne = "The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document"
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo = "1.Writing into an HTML element, using innerHTML <br>2.Writing into the HTML output using document.write() <br>3.Writing into an alert box, using window.alert() <br>4.Writing into the browser console, using console.log() ."
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerThree = "Using Javascript you can manipulate the HTML Elements querySelector(), querySelectorAll() amoung some others"
+document.getElementById("answerThree").innerHTML = answerThree;
